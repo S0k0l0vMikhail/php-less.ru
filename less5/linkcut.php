9845ac7b69a0a45775a930bf64ab6c672080d8b0
sokolov <dolgopeh@gmail.com>
2019-06-09 22:44:35 +0300

    upd
    upd

--- less5/linkcut.php
+++ less5/linkcut.php
@@ -3,7 +3,7 @@
 
 $link = 'https://yandex.ru/';
 //$link = 'https://yandex.ru/yandsearch?win=44&clid=37246&text=%D1%86%D1%8B%D0%BA%D0%BB&lr=2&redircnt=1560098511.1';
-$link = 'https://www.php.net/manual/ru/function.file-put-contents.php';
+//$link = 'https://www.php.net/manual/ru/function.file-put-contents.php';
 // $isLink = filter_var($link, FILTER_VALIDATE_URL);
 // $hash = md5($isLink);
 // var_dump($hash);
@@ -15,11 +15,9 @@ function cutlink($oglink){
   $flag = true;
   $isLink = filter_var($oglink, FILTER_VALIDATE_URL);
   if ($oglink && ($isLink != false)) { // проверяет что переменная переданна и что фильтрация прошла успешно
-    $res = file_get_contents($file, false, NULL, $offset);
-    var_dump($res);
+    $res = file_get_contents($file, false, NULL);
+    var_dump($res. " в файле перед всеми операциями");
     if ($res) { // проверяет что из файла пришли строки
-      //$flag = false;
-      //for ($offset=0; $offset < strlen($res) && $flag == false ; $offset+=49) { // цикл для перебора всех срок из файла, и получение нужной части строки после explode
       for ($offset = 0; $offset < strlen($res); $offset += 49) { // цикл для перебора всех срок из файла, и получение нужной части строки после explode
         var_dump(strlen($res). " длинна файла в при начале перебора");
         var_dump($offset. " offset при начале перебора");
@@ -29,8 +27,7 @@ function cutlink($oglink){
         $str = explode(":", $res);
         var_dump($str);
         var_dump("массив после explode");
-        //$i = 0;
-        //var_dump("i ". $i);
+      
         var_dump($str[0] . " результат перебора строк из файла");
         $hashFromFile = $str[0];
         var_dump("созданный в начале хэш: " . $hash . " и полученная из файла строка: " . $hashFromFile . " состояние флага " . $flag);
@@ -47,7 +44,7 @@ function cutlink($oglink){
         //var_dump($coupleLinks);
         file_put_contents($file, $coupleLinks, FILE_APPEND);
         //var_dump("your short link: " .$shortLink);
-        unset($flag);
+        //unset($flag);
       } else {
         echo "равен <br>";
         $shortLink = $str[1];
